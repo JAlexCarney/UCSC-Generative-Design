5c0e735e9b3dac9290e8be9884dc9b056470c33f
J. Alex Carney <38840263+JAlexCarney@users.noreply.github.com>
2019-06-28 18:13:45 -0700

    Added audio interaction.
    - Demo Song
    - Volume Slider
    - Emit Rate based on Volume

--- docs/Audio-Visualizer/sketch.js
+++ docs/Audio-Visualizer/sketch.js
@@ -1,27 +1,65 @@
 let particleSystem = null;
 let worldH = 700;
 let worldW = 700;
+let Demo = null;
+let amplitude = null;
+let DemoButton = null;
+let VolumeSlider = null;
+let volume = 0.5;
+
+function preload() {
+  soundFormats('mp3', 'ogg');
+  Demo = loadSound('Acoustic_Symphony.mp3');
+}
+
+function volumeChange(){
+  volume = volumeSlider.value * 0.01;
+  Demo.setVolume(volume);
+}
+
+function demo() {
+  if (Demo.isPlaying() ){
+    Demo.stop();
+    DemoButton.innerHTML = "Play Demo Audio";
+  } else {
+    Demo.play();
+    DemoButton.innerHTML = "Stop Demo Audio";
+  }
+}
 
 function setup() {
   let div = document.getElementById("content");
   let canvas = createCanvas(worldW, worldW);
   canvas.parent(div);
   noStroke();
+
+  Demo.setVolume(volume);
+  amplitude = new p5.Amplitude();
+
+  //find demo button
+  DemoButton = document.getElementById("demoButton");
+  //find volume slider
+  VolumeSlider = document.getElementById("volumeSlider");
   
   // create particle system
   let pos = createVector(worldW/2, worldH/2);
-  particleSystem = new ParticleSystem(pos, 30);
+  particleSystem = new ParticleSystem(pos, 40);
 }
 
 function draw() {
-	background("#facade");
-	
+	background("#FF7F2A");
+  fill("#E9AFAF");
+  circle(worldW/2, worldH/2, worldW);
+
   particleSystem.update();
   particleSystem.draw();
 
-  let emitChance = 0.2;
+  // set emit chance based on volume of audio.
+  let emitChance = (amplitude.getLevel()**2) * (1/volume);
 
-  if(Math.random() < emitChance){
-    particleSystem.emit();
+  for(let i = 0; i < 5; i++){
+    if(Math.random() < emitChance){
+      particleSystem.emit();
+    }
   }
 }
